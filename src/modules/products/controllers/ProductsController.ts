a8fda0d8ea0ca053919d7cd4485a04988e116735
Vicente <vicente19981@live.com>
2024-10-16 13:20:16 -0300

    feat: create show controller products

--- src/modules/products/controllers/ProductsController.ts
+++ src/modules/products/controllers/ProductsController.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
 import ListProductService from '../services/ListProductService';
+import ShowProductService from '../services/ShowProductService';
 
 export default class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listProducts = new ListProductService();
     const products = await listProducts.execute();
     return response.json(products);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showProduct = new ShowProductService();
+    const product = await showProduct.execute({ id });
+    return response.json(product);
   }
 }
