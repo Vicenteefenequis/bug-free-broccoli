5d5eb6a4585fddcd537e7e03ac61b26584cb1b61
Vicente <vicente19981@live.com>
2024-10-16 13:21:39 -0300

    feat: add create controller products

--- src/modules/products/controllers/ProductsController.ts
+++ src/modules/products/controllers/ProductsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import ListProductService from '../services/ListProductService';
 import ShowProductService from '../services/ShowProductService';
+import CreateProductService from '../services/CreateProductService';
 
 export default class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -13,6 +14,13 @@ export default class ProductsController {
     const { id } = request.params;
     const showProduct = new ShowProductService();
     const product = await showProduct.execute({ id });
+    return response.json(product);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+    const createProduct = new CreateProductService();
+    const product = await createProduct.execute({ name, price, quantity });
     return response.json(product);
   }
 }
